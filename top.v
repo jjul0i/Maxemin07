d6011be5307d597b20d5d8d886f7a7f964a88de2
jjul0i <josesilva47@estudante.ufscar.br>
2026-07-03 10:04:51 -0300

    Update top.v

--- top.v
+++ top.v
@@ -1,5 +1,6 @@
 module top (
     input x, y, z,
-    output f);
-    assign f = (~x | ~y | ~z) & (~x | ~y | z) & (x | ~y | z) & (~x | y | ~z);
+    output f
+);
+    assign f = (~x & ~y) | (~y & z) | (x & y & ~z);
 endmodule
